33e58dffec40847b8c11292e78be2bfd850b40eb
Luke Thompson <luke.thompson@nine.com.au>
2025-04-10 10:29:32 +1000

    removed unused param

--- src/app/api/check-status/route.ts
+++ src/app/api/check-status/route.ts
@@ -7,7 +7,7 @@ export const competitionHasClosed = (): boolean => {
   return now > closeDate
 }
 
-export async function GET(_: Request) {
+export async function GET() {
   try {
     if (competitionHasClosed()) {
       return NextResponse.json({ message: "Form is closed" }, { status: 400 })
