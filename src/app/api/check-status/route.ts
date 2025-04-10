06cdfe58ae7a7c7fc1e35ff35ac6fbc514629372
Luke Thompson <luke.thompson@nine.com.au>
2025-04-10 10:42:48 +1000

    renamed param

--- src/app/api/check-status/route.ts
+++ src/app/api/check-status/route.ts
@@ -7,9 +7,8 @@ export const competitionHasClosed = (): boolean => {
   return now > closeDate
 }
 
+// @ts-ignore - Required by Next.js Route Handler
 export async function GET(_request: Request) {
-  _request
-
   try {
     if (competitionHasClosed()) {
       return NextResponse.json({ message: "Form is closed" }, { status: 400 })
