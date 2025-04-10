a80b81d6f967b710cfd1473fb1ae5a0e5460f1ef
Luke Thompson <luke.thompson@nine.com.au>
2025-04-10 10:52:58 +1000

    unused parameter debugging

--- src/app/api/check-status/route.ts
+++ src/app/api/check-status/route.ts
@@ -1,5 +1,5 @@
 import { competitionHasClosed } from "@/utilities"
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
 export async function GET() {
   try {
