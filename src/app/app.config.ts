14c625ce9a1d7d79ca2482867d616efc108d1dcb
LukeT2340 <luke.thompson@nine.com.au>
2025-04-11 13:02:56 +1000

    edit close date

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 const config = {
-  allowMultipleSubmissions: false,
-  closeDate: new Date("2025-04-10T17:00:00+10:00"),
-  immersiveTitle: "BRAND - HEADING - SUBHEADING - MASTHEAD",
-  immersiveDescription: "IMMERSIVE DESCRIPTION",
-  canonical: "https://brandedcontent.smh.com.au/brand/heading",
-  socialShareImage:
-    "https://brandedcontent.smh.com.au/inc/social/SOCIAL-SHARE-IMAGE.JPG",
-  masthead: "smh",
-  themeColor: "#0A1633",
+	allowMultipleSubmissions: false,
+	closeDate: new Date("2025-04-11T17:00:00+10:00"),
+	immersiveTitle: "BRAND - HEADING - SUBHEADING - MASTHEAD",
+	immersiveDescription: "IMMERSIVE DESCRIPTION",
+	canonical: "https://brandedcontent.smh.com.au/brand/heading",
+	socialShareImage:
+		"https://brandedcontent.smh.com.au/inc/social/SOCIAL-SHARE-IMAGE.JPG",
+	masthead: "smh",
+	themeColor: "#0A1633",
 }
 
 export default config
