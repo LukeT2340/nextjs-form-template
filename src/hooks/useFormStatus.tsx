1c0721f68b13e4d8567d4848bda114f143f796d7
Luke Thompson <luke.thompson@nine.com.au>
2025-04-14 10:54:27 +1000

    documentation

--- src/hooks/useFormStatus.tsx
+++ src/hooks/useFormStatus.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 /**
  * @description Custom hook to check if the competition is open. Define close date in the api.config.ts config.
- * @returns { formIsOpen: boolean; checkingStatus: boolean; } - Returns true if the form is open, false otherwise.
+ * @returns { formIsOpen: boolean; checkingStatus: boolean; }
+ * - formIsOpen: true if the form is open, false if closed;
+ * - checkingStatus: true if the status is being checked, false otherwise.
  */
 const useFormStatus = (): { formIsOpen: boolean; checkingStatus: boolean } => {
-  const [formIsOpen, setFormIsOpen] = useState<boolean>(false)
-  const [checkingStatus, setCheckingStatus] = useState<boolean>(true)
+  const [formIsOpen, setFormIsOpen] = useState<boolean>(false);
+  const [checkingStatus, setCheckingStatus] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchFormStatus = async () => {
       try {
-        const response = await axios.get("/api/check-status")
-        setFormIsOpen(response.status === 200)
+        const response = await axios.get("/api/check-status");
+        setFormIsOpen(response.status === 200);
       } catch (error) {
-        setFormIsOpen(false)
-        console.error("Error fetching form status:", error)
+        setFormIsOpen(false);
+        console.error("Error fetching form status:", error);
       } finally {
-        setCheckingStatus(false)
+        setCheckingStatus(false);
       }
-    }
+    };
 
-    fetchFormStatus()
-  }, [])
+    fetchFormStatus();
+  }, []);
 
-  return { formIsOpen, checkingStatus }
-}
+  return { formIsOpen, checkingStatus };
+};
 
-export default useFormStatus
+export default useFormStatus;
