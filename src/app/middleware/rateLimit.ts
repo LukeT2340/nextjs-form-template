f92da98de82cb52f8f62b0ad1607d6a5844cdbc0
Luke Thompson <luke.thompson@nine.com.au>
2025-04-14 17:08:06 +1000

    rate limiting

--- src/app/middleware/rateLimit.ts
+++ src/app/middleware/rateLimit.ts
@@ -11,7 +11,7 @@ export default function rateLimitMiddleware(handler) {
     }
 
     const ip = req.headers["x-vercel-forwarded-for"];
-    console.log(req.headers["x-vercel-forwarded-for"]);
+    console.log(req.headers);
     console.log(ip);
     if (!ip || ip.length === 0) {
       return NextResponse.json(
