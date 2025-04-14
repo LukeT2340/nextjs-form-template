634b435efc19ecc0dfc500b696a27255cd4f2cd0
Luke Thompson <luke.thompson@nine.com.au>
2025-04-14 10:45:13 +1000

    documentation

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,8 +1,14 @@
 import { toast } from "react-toastify";
 import { FormData } from "@/components/Form/form-schema";
 
+/**
+ * @description Handles form submission by sending a POST request to the server.
+ * @param {FormData} data - The form data to be submitted.
+ * @returns {Promise<boolean>} - Returns true if the submission was successful, false otherwise.
+ */
 export const handleSubmitForm = async (data: FormData): Promise<boolean> => {
   try {
+    // Make post request to server
     const response = await fetch("/api/submit", {
       method: "POST",
       headers: {
@@ -11,7 +17,10 @@ export const handleSubmitForm = async (data: FormData): Promise<boolean> => {
       body: JSON.stringify(data),
     });
 
+    // Parse the response
     const dataResponse = await response.json();
+
+    // Handle bad response
     if (!response.ok) {
       console.error(
         `Error submitting form: ${JSON.stringify(dataResponse.error)}`
@@ -20,6 +29,7 @@ export const handleSubmitForm = async (data: FormData): Promise<boolean> => {
       return false;
     }
 
+    // Handle success response
     toast.success("Form submitted successfully", {
       position: "top-center",
       autoClose: 3000,
